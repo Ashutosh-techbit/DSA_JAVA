361f23a7ca8b064b929e2550767d48f2a2bce90a
Ashutosh bansal <bansalashutosh77@gmail.com>
2024-11-09 21:52:13 +0530

    d

---
+++ 13_Array/ReverseArray.java
@@ -0,0 +1,24 @@
+import java.util.*;;
+
+public class ReverseArray {
+    public static void reverse(int nums[]){
+        int start =0 , end= nums.length-1;
+        while(start<end){
+            int temp = nums[end];
+            nums[end] = nums[start];
+            nums[start] = temp;  
+            start++;
+            end--;
+        }
+       
+    }
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,4,5,6};
+        reverse(nums);     
+
+        for(int i=0;i<nums.length;i++){
+            System.out.println(nums[i]);
+        }
+
+    }
+}
