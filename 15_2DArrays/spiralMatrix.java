47a3354ed33b934beb40aec24de94100a8384e0c
Ashutosh bansal <bansalashutosh77@gmail.com>
2024-11-15 18:54:18 +0530

    spiralmatrix

--- 15_2DArrays/spiralMatrix.java
+++ 15_2DArrays/spiralMatrix.java
@@ -26,16 +26,17 @@ public class spiralMatrix {
                 System.out.print(nums[maxrow][i] + " ");
             }
             System.out.println();
-
+            
             //left
             for(int i=maxrow-1;i>=minrow;i--)
             {
                 System.out.print(nums[i][mincol] + " ");
             }
+            System.out.println();
 
          }
     }
-    
+
     public static void main(String[] args) {
         int nums[][]={{0,1,2,3},{4,5,6,7},{8,9,10,11},{12,13,14,15}};
 
