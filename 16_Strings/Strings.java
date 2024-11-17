f9246864fd8b708659c03df5b192e9a78abfc2c0
Ashutosh bansal <bansalashutosh77@gmail.com>
2024-11-17 10:15:49 +0530

    Strings

---
+++ 16_Strings/Strings.java
@@ -0,0 +1,14 @@
+//Strings are immutable
+
+
+public class Strings {
+    public static void main(String[] args) {
+        char arr[] = {'a','b','c'};
+        String s= "abcd";
+        String z=new String("hello world");
+
+        System.out.println(arr);
+        System.out.println(s);
+        System.out.println(arr[0]);
+    }    
+}
