957b66483783390f35bed3d3353b04c3204b621b
Ashutosh bansal <bansalashutosh77@gmail.com>
2024-11-18 20:22:32 +0530

    in-built fucntion

--- 16_Strings/Strings.java
+++ 16_Strings/Strings.java
@@ -1,14 +1,35 @@
 //Strings are immutable
 
+import java.util.Scanner;
 
 public class Strings {
     public static void main(String[] args) {
+
+        //Intialization
         char arr[] = {'a','b','c'};
         String s= "abcd";
         String z=new String("hello world");
 
-        System.out.println(arr);
-        System.out.println(s);
-        System.out.println(arr[0]);
+        //take input
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next(); //take string of one word
+        String name1 = sc.nextLine(); //take full line
+
+        // System.out.print(name); //ashu
+        // System.out.print(name1); //ashu bansal
+
+
+        //in-built function
+        // String str = "ashu";
+        // System.out.println(str.length());
+      
+        //Concatenation
+       String firstname = "ashu";
+       String Lastname = "bansal";
+       String fullname = firstname+" " + Lastname;
+       System.out.println(fullname);
+       
+
+
     }    
 }
