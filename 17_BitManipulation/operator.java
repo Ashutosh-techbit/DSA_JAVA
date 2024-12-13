57cc1f293b68b72eb289b9b9c655f96842e2beb7
Ashutosh bansal <bansalashutosh77@gmail.com>
2024-12-13 09:57:42 +0530

    Bit manipulation

---
+++ 17_BitManipulation/operator.java
@@ -0,0 +1,11 @@
+public class operator {
+    public static void main(String[] args) {
+        System.out.println(5 & 9); //AND
+        System.out.println(5 | 9); //OR
+        System.out.println(5 ^ 6); //XOR
+        System.out.println(~12); //Binary complement
+        System.out.println(5<<2); //Binary left shift
+        System.out.println(5>>2); //Binary right shift
+
+    }
+}
