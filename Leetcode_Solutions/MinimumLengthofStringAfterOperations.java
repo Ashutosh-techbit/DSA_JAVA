484621f73ef15b94502d3892db60665f3bef16b6
Ashutosh bansal <bansalashutosh77@gmail.com>
2025-01-14 00:06:14 +0530

    RECURSION

---
+++ Leetcode_Solutions/MinimumLengthofStringAfterOperations.java
@@ -0,0 +1,29 @@
+public class MinimumLengthofStringAfterOperations {
+
+    public static 
+    public static void main(String[] args) {
+
+        String s="abaacbcbb"; 
+
+
+    }
+}
+
+
+// class Solution {
+//     public int minimumLength(String s) {
+//         if(s.length()<3){
+//             return s.length();
+//         }
+
+//         StringBuilder sb =  new StringBuilder("");
+//         for(int i=0;i<s.length();i++){
+//             sb.append(s.charAt(i));
+//         }
+
+//         for(int i=1;i<sb.length()-1;i++){
+            
+//         }
+//         return s.length();
+//     }
+// }
---
+++ Recursion/basics.java
@@ -0,0 +1,16 @@
+public class basics{
+   
+    //recursion 
+    static void print(int n){
+        //base condition to stop recursion
+         if(n==6){
+              return;
+         }
+       System.out.println(n);
+       print(n+1);
+    }
+    public static void main(String[] args) {
+        int n=1;
+        print(n);
+    }
+}
