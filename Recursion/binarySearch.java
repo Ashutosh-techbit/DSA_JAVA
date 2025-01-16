36951bb3c6b4b391397b3dbd058f5ea35596d32b
Ashutosh bansal <bansalashutosh77@gmail.com>
2025-01-16 15:38:42 +0530

    binarysearch

---
+++ Recursion/binarySearch.java
@@ -0,0 +1,27 @@
+public class binarySearch {
+    public static int binsearch(int arr[] , int target , int start ,  int end){
+        if(start>end){
+            return -1;
+        }
+        int mid = (start +(end - start)/2);
+        if(arr[mid]==target){
+            return mid;
+        }
+
+        else if(target < arr[mid]){
+            end=mid-1;
+           return binsearch(arr, target, start, end);
+        }
+        else{
+            start=mid+1;
+            return binsearch(arr, target, start, end);
+        }
+        
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,3,13,23,53}; //sorted
+        int target = 13;
+        int k = binsearch(arr, target, 0, arr.length-1);
+        System.out.println(k);
+    }
+}
