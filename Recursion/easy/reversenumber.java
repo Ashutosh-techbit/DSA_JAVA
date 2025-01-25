62e7702d7691dcecefa1dd9b2d83cea4b1db7206
Ashutosh bansal <bansalashutosh77@gmail.com>
2025-01-25 13:15:40 +0530

    palindrome

--- Recursion/easy/reversenumber.java
+++ Recursion/easy/reversenumber.java
@@ -1,16 +1,16 @@
 public class reversenumber {
     static int sum=0;
-    public static void rev1(int n){
+    public static void rev(int n){
         if(n==0){
             return;
         }
         int rem = n%10;
         sum=sum*10 + rem;
-        rev1(n/10);
+        rev(n/10);
 
     }
  public static void main(String[] args) {
-     rev1(1875);
+     rev(1875);
      System.out.println(sum);   
  }
     
