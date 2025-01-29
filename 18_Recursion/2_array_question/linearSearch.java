1f8465f9715ff8e1d08d91ba54fc9d30cea496bb
Ashutosh bansal <bansalashutosh77@gmail.com>
2025-01-29 17:27:49 +0530

    multiple occurencee

--- 18_Recursion/2_array_question/linearSearch.java
+++ 18_Recursion/2_array_question/linearSearch.java
@@ -1,36 +1,55 @@
+import java.util.ArrayList;
+
 public class linearSearch {
 
-    //check element exists or not
-    public static boolean linearSearch(int[] arr , int target , int index){
-        if(index == arr.length){
+    // check element exists or not
+    public static boolean linearSearch(int[] arr, int target, int index) {
+        if (index == arr.length) {
             return false;
         }
 
-        if(arr[index]==target){
+        if (arr[index] == target) {
             return true;
-        }
-        else{
-            return linearSearch(arr, target, index+1);
+        } else {
+            return linearSearch(arr, target, index + 1);
         }
     }
-
-    //index of element
-    public static int findindex(int[] arr , int target , int index){
-        if(index == arr.length){
+    
+    
+    // index of element
+    public static int findindex(int[] arr, int target, int index) {
+        if (index == arr.length) {
             return -1;
         }
 
-        if(arr[index]==target){
+        if (arr[index] == target) {
             return index;
-        }
-        else{
-            return findindex(arr, target, index+1);
+        } else {
+            return findindex(arr, target, index + 1);
         }
     }
+   
 
-     public static void main(String[] args) {
-        int[] arr ={1,34,65,13,12};
+    // returns index of multiple occurence of element if exists 
+    public static ArrayList<Integer> findallindex(int[] arr, int target, int index  , ArrayList<Integer> list) {
+        if (index == arr.length) {
+            return list;
+        }
+
+        if (arr[index] == target) {
+            list.add(index);
+         }
+        return findallindex(arr, target, index + 1,list);
+        
+        
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 34, 65, 13, 12 ,12};
         System.out.println(linearSearch(arr, 65, 0));
         System.out.println(findindex(arr, 65, 0));
-     }    
+
+       ArrayList<Integer> ans = findallindex(arr, 12, 0,new ArrayList<Integer>());
+        System.out.println(ans);
+    }
 }
