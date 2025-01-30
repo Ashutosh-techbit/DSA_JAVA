6aa3c60e3a070ccf95cf5df07ab91cb4e25daf9f
Ashutosh bansal <bansalashutosh77@gmail.com>
2025-01-30 14:31:18 +0530

    patterns using recursion

---
+++ 18_Recursion/pattern.java
@@ -0,0 +1,46 @@
+public class pattern {
+    // r - row , c - columns
+
+    // ****
+    // ***
+    // **
+    // *
+    public static void traingle1(int r , int c){
+        if(r == 0){
+            return;
+        }
+
+        if(c<r){
+            System.out.print("*");
+            traingle1(r, c+1);
+        }
+        else{
+            System.out.println();
+            traingle1(r-1, 0);
+        }
+    }
+
+
+    // *
+    // **
+    // ***
+    // ****
+    public static void traingle2(int r , int c){
+        if(r == 0){
+            return;
+        }
+
+        if(c<r){
+            traingle2(r, c+1);
+            System.out.print("*");
+        }
+        else{
+            traingle2(r-1, 0);
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        traingle1(4,0 );
+        traingle2(4,0 );
+    }
+}
