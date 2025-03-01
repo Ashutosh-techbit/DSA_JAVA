916272d7f3ee4c49de974ecab9c53d9b7cb7100a
Ashutosh bansal <bansalashutosh77@gmail.com>
2025-03-01 11:35:58 +0530

    doubly linked list insert and display

---
+++ Linked_list/DLL.java
@@ -0,0 +1,44 @@
+
+// ==================================== DOUBLY LINKED LIST ======================================
+package Linked_list;
+
+public class DLL {
+    private Node head;
+
+    // ==================================== node of a doubly linked list =================================================
+    private class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node prev, Node next) {
+            this.value = value;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+        node.next=head;
+        node.prev = null;
+        if(head != null){
+            head.prev=node;
+        }
+        head = node;
+    }
+
+    // ======================================= DISPLAY ===============================
+    public void display(){
+         Node node = head;
+         while(node.next != null){
+            System.out.print(node.value + " -> ");
+            node = node.next;
+         }
+         System.out.println("END");
+    }
+}
---
+++ Linked_list/DLLmain.java
@@ -0,0 +1,16 @@
+package Linked_list;
+
+public class DLLmain {
+    public static void main(String[] args) {
+        DLL dlist = new DLL();
+        dlist.insertFirst(12); 
+        dlist.insertFirst(13); 
+        dlist.insertFirst(14); 
+        dlist.insertFirst(15); 
+        dlist.insertFirst(16); 
+        dlist.insertFirst(17); 
+
+        dlist.display();
+        
+    }
+}
